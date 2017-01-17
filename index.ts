91d89c9c2afef70994e2973b425ff06bff16fda7
CFitzsimons <colin.fitzsimons3@mail.dcu.ie>
2017-01-17 20:05:59 +0000

    Creates and runs discord client

--- index.ts
+++ index.ts
@@ -1,17 +1,5 @@
-const constants = require("./configuration/constants.json");
-import { DiceRoller } from "./executor/actions";
+import DiscordClient from "./client/client";
 
+let client = new DiscordClient();
 
-
-import Discord from "./connection/discord";
-
-
-let client = new Discord();
-
-client.addHandler( (event: any) => {
-  console.log(event.content);
-  if(event.content === "ping")
-    event.channel.sendMessage("pong")
-}, "main_client");
-
-client.connect(constants.bot_token);
+client.start();
